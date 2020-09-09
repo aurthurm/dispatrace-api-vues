669198bdd57c55f51b94ef31504d1c5a49ab11f2
Aurthur Musendame <aurthurmusendame@gmail.com>
2020-09-09 22:17:30 +0200

    added build-essential

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,12 +1,12 @@
 echo "Running Django Migrations ...";
 
-python ./manage.py makemigrations
+python backend/manage.py makemigrations
 
-until python ./manage.py migrate; do
+until python manage.py migrate; do
   sleep 2
   echo "Retrying to migrate db ... ";
 done
 
-python ./manage.py shell < init_admin.py
+python /app/backend/manage.py shell < init_admin.py
 
 echo "Django App is ready ....";
