d28951430f8e0e76defada10ecece00585c6faf6
Aurthur Musendame <aurthurmusendame@gmail.com>
2020-09-10 01:08:54 +0200

    left a .

--- backend/dispatrace/settings.py
+++ backend/dispatrace/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'dispatrace.middleware.CORSMiddleware'
+    'dispatrace.middleware.CORSMiddleware',
     'dispatrace.middleware.CustomWhiteNoiseMiddleware'
 ]
 
