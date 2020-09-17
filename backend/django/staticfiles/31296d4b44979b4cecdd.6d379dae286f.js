d4af3f0fd50c17b4e8612c8f5a58df4820379c6d
Aurthur Musendame <aurthurmusendame@gmail.com>
2020-09-17 22:41:28 +0200

    updated compose

--- backend/django/staticfiles/31296d4b44979b4cecdd.6d379dae286f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{441:function(t,e,n){"use strict";n.r(e);var o={mounted:function(){this.$store.dispatch("memorandums/setMemoDetailView",!0)}},c=n(28),component=Object(c.a)(o,(function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("nuxt-child")],1)}),[],!1,null,null,null);e.default=component.exports}}]);
