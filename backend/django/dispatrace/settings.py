170a2b9121235112e1dd93e1f8529732f4888fb0
Aurthur Musendame <aurthurmusendame@gmail.com>
2020-10-12 15:57:38 +0200

    updated settings

--- backend/django/dispatrace/settings.py
+++ backend/django/dispatrace/settings.py
@@ -98,23 +98,23 @@ WSGI_APPLICATION = 'dispatrace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {   #TODO:  Use env to get these from environ
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'smartworks_db',
-#         'USER': 'smartworks_admin',
-#         'PASSWORD': 'SmartWorks1234',
-#         'HOST': 'smartworks_db',
-#         'PORT': 5432,
-#     }
-# }
-
-DATABASES = {
+DATABASES = {   #TODO:  Use env to get these from environ
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'smartworks_db',
+        'USER': 'smartworks_admin',
+        'PASSWORD': 'SmartWorks1234',
+        'HOST': 'smartworks_db',
+        'PORT': 5432,
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
